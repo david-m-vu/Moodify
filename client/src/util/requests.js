333b8ad73e6c97f9e7cd218c459165f71b82aa14
David Vu <davidvu7887@gmail.com>
2023-07-03 01:17:21 -0700

    Setting up gh-pages

--- client/src/util/requests.js
+++ client/src/util/requests.js
@@ -1,6 +1,6 @@
 import { formatEmotionScores } from "./util.js"
 
-const baseURL = "http://127.0.0.1:5000";
+const baseURL = "https://moodify-backend-t9di.onrender.com";
 
 const sampleExplanations = [
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -25,13 +25,13 @@ const tempEmotions = [{
 }];
 
 export const getExplanations = async (id) => {
-  let response = await fetch(`${baseURL}/gptexplain/${id}`);
-  let responseJSON;
-  if (response.ok) {
-      responseJSON = await response.json();
-  }
-  return responseJSON.items;
-  // return sampleExplanations;
+  // let response = await fetch(`${baseURL}/gptexplain/${id}`);
+  // let responseJSON;
+  // if (response.ok) {
+  //     responseJSON = await response.json();
+  // }
+  // return responseJSON.items;
+  return sampleExplanations;
 };
 
 const wait = (time) => {
