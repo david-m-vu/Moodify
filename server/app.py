f1921d6bda57d02dc32288d2e413c9badeb9425a
David Vu <davidvu7887@gmail.com>
2023-07-04 22:27:40 -0700

    Disabled localhost from cors origin

--- server/app.py
+++ server/app.py
@@ -5,7 +5,8 @@ from routes import main
 
 def create_app():
     app = Flask(__name__) 
-    CORS(app, origins=["https://david-m-vu.github.io", "http://localhost:3000"])
+    # when running website in localhost, add http://localhost:3000 to origins
+    CORS(app, origins=["https://david-m-vu.github.io"])
 
     app.register_blueprint(main)
 
