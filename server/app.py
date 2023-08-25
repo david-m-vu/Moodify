713248407f0df39e2f4fce8efd1ef6a2c1039cb4
David Vu <davidvu7887@gmail.com>
2023-08-25 00:07:21 -0700

    trying to fix cors issue

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from routes import main
 def create_app():
     app = Flask(__name__) 
     # when running website in localhost, add http://localhost:3000 to origins
-    CORS(app, origins=["https://david-m-vu.github.io"])
+    CORS(app)
 
     app.register_blueprint(main)
 
